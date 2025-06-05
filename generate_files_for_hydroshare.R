df76a4333622a75763c80cf31b6b75a85d5ea875
Mike Vlah <vlahm13@gmail.com>
2025-06-05 15:57:09 -0600

    breaking upstream pulse files by region

--- generate_files_for_hydroshare.R
+++ generate_files_for_hydroshare.R
@@ -1,9 +1,9 @@
-#procedure:
-#1. download everything in bulk_download_files on server
-#2. unzip everything (unzip '*.zip')
-#3. delete or move all zip archives (rm *.zip)
-#4. run this script to generate hydroshare files
-#5. upload those files, making sure they are registered as new versions of previously uploaded files
+# procedure:
+# 1. download everything in bulk_download_files on server
+# 2. unzip everything (unzip '*.zip')
+# 3. delete or move all zip archives (rm *.zip)
+# 4. run this script to generate hydroshare files
+# 5. upload those files, making sure they are registered as new versions of previously uploaded files
 
 
 library(tidyverse)
@@ -12,89 +12,108 @@ library(EMLassemblyline)
 library(glue)
 # library(EDIutils)
 
-wd <- '/home/mike/git/streampulse/dataset_build/bulk_download_files'
-md <- '../metadata'
+wd <- "/home/mike/git/streampulse/dataset_build/bulk_download_files"
+md <- "../metadata"
 
 setwd(wd)
 dir.create(md)
 
-#uncomment this. only commented for safety during development
+# uncomment this. only commented for safety during development
 # for(f in list.files(wd)){
 #     file.rename(f, sub('^all_', '', f))
 # }
 
-file.remove('model_input_summary_data.csv') #not worth including
+file.remove("model_input_summary_data.csv") # not worth including
 
-#these lines only need to be run once. template files are then manually edited.
+# these lines only need to be run once. template files are then manually edited.
 # template_table_attributes(md, wd, 'basic_site_data.csv')
 # template_table_attributes(md, wd, 'daily_model_results.csv')
 # template_table_attributes(md, wd, 'grab_data.csv')
-template_table_attributes(md, wd, 'model_summary_data.csv')
+template_table_attributes(md, wd, "model_summary_data.csv")
 
-read_csv('grab_data.csv') %>% 
+read_csv("grab_data.csv") %>%
     mutate(
         unit = case_match(
             variable,
-            'TOC' ~ 'ppm',
-            'TN' ~ 'ppm',
-            'TP' ~ 'ppm',
-            'TDN' ~ 'mg/L',
-            'TDP' ~ 'mg/L',
-            'SRP' ~ 'mg/L',
-            'DOC' ~ 'ppm',
-            'DIC' ~ 'ppm',
-            'TSS' ~ 'ppm',
-            'fDOM' ~ 'ppb',
-            'Carbon dioxide' ~ 'ppm',
-            'CO2' ~ 'ppm',
-            'Methane' ~ 'ug/L',
-            'CH4' ~ 'ug/L',
-            'Nitrous oxide' ~ 'ug/L',
-            'N2O' ~ 'ug/L',
-            'DO' ~ 'mg/L',
-            'DO Sat' ~ '%',
-            'DO_Sat' ~ '%',
-            'Chlorophyll-a' ~ 'mg/L',
-            'Chl-a' ~ 'mg/L',
-            'Alkalinity' ~ 'meq/L',
-            'pH' ~ 'pH units',
-            'Spec Cond' ~ 'mS/cm',
-            'Spec_Cond' ~ 'mS/cm',
-            'Turbidity' ~ 'NTU',
-            'Light Atten.' ~ '1/m',
-            'Light_Atten' ~ '1/m',
-            'Illuminance' ~ 'lux',
-            'PAR' ~ 'W/m^2',
-            'UV Absorbance' ~ '1/cm',
-            'UV_Absorbance' ~ '1/cm',
-            'Canopy Cover' ~ 'LAI',
-            'Canopy_Cover' ~ 'LAI',
-            'Width' ~ 'm',
-            'Depth' ~ 'm',
-            'Distance' ~ 'm',
-            'Discharge' ~ 'm^3/s',
-            'k' ~ '1/min',
-            'Water Temp' ~ 'C',
-            'Water_Temp' ~ 'C',
-            'Air Temp' ~ 'C',
-            'Air_Temp' ~ 'C',
-            'Water Pres' ~ 'kPa',
-            'Water_Pres' ~ 'kPa',
-            'Air Pres' ~ 'kPa',
-            'Air_Pres' ~ 'kPa'
+            "TOC" ~ "ppm",
+            "TN" ~ "ppm",
+            "TP" ~ "ppm",
+            "TDN" ~ "mg/L",
+            "TDP" ~ "mg/L",
+            "SRP" ~ "mg/L",
+            "DOC" ~ "ppm",
+            "DIC" ~ "ppm",
+            "TSS" ~ "ppm",
+            "fDOM" ~ "ppb",
+            "Carbon dioxide" ~ "ppm",
+            "CO2" ~ "ppm",
+            "Methane" ~ "ug/L",
+            "CH4" ~ "ug/L",
+            "Nitrous oxide" ~ "ug/L",
+            "N2O" ~ "ug/L",
+            "DO" ~ "mg/L",
+            "DO Sat" ~ "%",
+            "DO_Sat" ~ "%",
+            "Chlorophyll-a" ~ "mg/L",
+            "Chl-a" ~ "mg/L",
+            "Alkalinity" ~ "meq/L",
+            "pH" ~ "pH units",
+            "Spec Cond" ~ "mS/cm",
+            "Spec_Cond" ~ "mS/cm",
+            "Turbidity" ~ "NTU",
+            "Light Atten." ~ "1/m",
+            "Light_Atten" ~ "1/m",
+            "Illuminance" ~ "lux",
+            "PAR" ~ "W/m^2",
+            "UV Absorbance" ~ "1/cm",
+            "UV_Absorbance" ~ "1/cm",
+            "Canopy Cover" ~ "LAI",
+            "Canopy_Cover" ~ "LAI",
+            "Width" ~ "m",
+            "Depth" ~ "m",
+            "Distance" ~ "m",
+            "Discharge" ~ "m^3/s",
+            "k" ~ "1/min",
+            "Water Temp" ~ "C",
+            "Water_Temp" ~ "C",
+            "Air Temp" ~ "C",
+            "Air_Temp" ~ "C",
+            "Water Pres" ~ "kPa",
+            "Water_Pres" ~ "kPa",
+            "Air Pres" ~ "kPa",
+            "Air_Pres" ~ "kPa"
         ),
-        unit = if_else(is.na(unit), 'mole', unit)
-    ) %>% 
-    relocate('unit', .after = 'value') %>% 
-    write_csv('grab_data.csv')
+        unit = if_else(is.na(unit), "mole", unit)
+    ) %>%
+    relocate("unit", .after = "value") %>%
+    write_csv("grab_data.csv")
 
-read_csv('model_summary_data.csv') %>% 
-    select(-requested_variables, -year, -engine, -used_rating_curve,
-           -GPP_95CI, -ER_95CI, -current_best) %>% 
-    write_csv('model_summary_data.csv')
+read_csv("model_summary_data.csv") %>%
+    select(
+        -requested_variables, -year, -engine, -used_rating_curve,
+        -GPP_95CI, -ER_95CI, -current_best
+    ) %>%
+    write_csv("model_summary_data.csv")
 
-neon_data <- purrr::map_dfr(list.files(pattern = '^neon'),
-                            read_csv)
-neon_data <- 
+    sp_data <- purrr::map_dfr(
+        list.files(pattern = "^sp_data"),
+        read_csv
+    )
 
-file.remove('../metadata/custom_units.txt')
+    for (region in unique(sp_data$regionID)) {
+        sp_data %>%
+            filter(regionID == !!region) %>%
+            arrange(siteID, dateTimeUTC) %>%
+            write_csv(glue("inputdata_streampulse_region", region, ".csv"))
+    }
+
+    # neon_data <- purrr::map_dfr(list.files(pattern = '^neon'),
+    #                             read_csv)
+    #
+    # for(site in unique(neon_data$siteID)){
+    #     neon_data <- neon_data %>% arrange
+    # }
+
+# feather::write_feather(neon_data, "../neon_combined.feather")
+
+file.remove("../metadata/custom_units.txt")
